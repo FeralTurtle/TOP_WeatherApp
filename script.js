1dd5856f69250104ac6e52023b7e0e6f1705eeda
Jeremy T <jeremytupas@gmail.com>
2022-04-14 11:19:36 -0500

    Fetch weather of correct location based on user input which can be city/state or by ZIP code

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 async function getWeather() {
+    const locationInput = document.querySelector('#location-input');
+    const enteredLocation = locationInput.value;
+    const alphabetical = /[A-Za-z]/;
+    const fiveNums = /\d{5}/;
+
     try {
-        const locationInput = document.querySelector('#location-input');
-        const enteredLocation = locationInput.value;
-    
-        const responseObj = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${enteredLocation}&appid=145f93b6e84a1bae5151459d8af682a9`, { mode: 'cors' });
+        let responseObj;
+        if (alphabetical.test(enteredLocation)) {
+            console.log('is a string')
+            responseObj = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${enteredLocation}&appid=145f93b6e84a1bae5151459d8af682a9`, { mode: 'cors' });
+        } else if (fiveNums.test(enteredLocation)) {
+            console.log('is a number')
+            responseObj = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${enteredLocation}&appid=145f93b6e84a1bae5151459d8af682a9`, { mode: 'cors' });
+        };
         console.log(responseObj);
         const jsObj = await responseObj.json();
         console.log(jsObj);
@@ -15,3 +24,11 @@ async function getWeather() {
 
 const locationBtn = document.querySelector('#location-btn');
 locationBtn.addEventListener('click', getWeather);
+
+// const moreThanFiveNums = /\d{6,}/;
+// const locationInput = document.querySelector('#location-input');
+// locationInput.addEventListener('input', () => {
+//     if (moreThanFiveNums) {
+//         console.log('more than five nums');
+//     };
+// });
